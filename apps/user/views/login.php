4c6baed42ea072d388ae222c760888e04286337a
Thibault Vlacich <thibault.vlacich@gmail.com>
2015-12-02 21:36:23 +0100

    Add the user app

---
+++ apps/user/views/login.php
@@ -0,0 +1,15 @@
+<div class="app-user app-user-login">
+  <h2>Connexion</h2>
+  <form id="Connexion" method="post" action="SUPERPAGEHTML.html">
+    <div>
+      <label for="email">Email</label><input name="email" id="email" type="text" placeholder="exemple@domaine.com" autofocus required /><br/>
+      <label for="password">Mot de Passe</label><input id="password" type="password" required /><br/>
+      <input type="submit" value="Se Connecter" />
+    </div>
+  </form>
+  <div class="links">
+    <a href="Pagemotdepasseoublié" class="password-lost">Mot de passe oublié</a>
+    <a href="Pagepasencoreinscrit" class="register">Pas encore inscrit ?</a>
+    <div class="clearer"></div>
+  </div>
+</div>
