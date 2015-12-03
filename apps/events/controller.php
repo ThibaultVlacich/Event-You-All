46e83a0627b0081682c464bd3fa71647a39f226f
leoplouvier <plouvierleo@gmail.com>
2015-12-03 12:10:11 +0100

    probleme with redirection to event page

---
+++ apps/events/controller.php
@@ -0,0 +1,36 @@
+<?php
+
+class UserController extends Controller {
+  var $default_module = 'lol';
+
+  /**
+	 * The Signup action allows a user to register a new account.
+	 *
+	 * @return array Data given
+	 */
+   function login() {
+     return array('pseudo' => 'lol');
+   }
+
+  /**
+	 * The Signup action allows a user to register a new account.
+	 *
+	 * @return array Data given
+	 */
+  function signup() {
+
+    $data = Request::getAssoc(array('pseudonyme', 'password', 'password_confirm', 'firstname', 'lastname', 'date_naissance', 'date_inscription', 'sex', 'email', 'telephone', 'access', 'adresse', 'code_postal', 'ville'));
+
+    if (!in_array(null, $data, true)) {
+      $errors = array();
+
+      if($data['password'] == $data['password_confirm']) {
+        $data['password'] = sha1($data['password']);
+      }
+    } else {
+      include 'views/signup.php';
+    }
+  }
+}
+
+?>
---
+++ apps/events/model.php
@@ -0,0 +1,48 @@
+<?php
+
+class UserModel {
+
+  protected $db;
+
+  public function __construct() {
+    $this->db = System::getDb();
+  }
+
+
+  	/**
+  	 * Creates a user in the database.
+  	 *
+  	 * @param array $data
+  	 * @return mixed ID of the user just created or false on failure
+  	 */
+  	public function createUser(array $data) {
+  		$prep = $this->db->prepare('
+  			INSERT INTO users(pseudonyme, password, firstname, lastname, date_naissance, date_inscription, sex, email, telephone, access, adresse, code_postal, ville)
+  			VALUES (:pseudonyme, :password, :firstname, :lastname, :date_naissance, :date_inscription, :sex, :email, :telephone, :access, :adresse, :code_postal, :ville)
+  		');
+
+      $prep->bindParam(':pseudonyme', $data['pseudonyme']);
+      $prep->bindParam(':password', $data['password']);
+      $prep->bindParam(':firstname', $data['firstname']);
+      $prep->bindParam(':lastname', $data['lastname']);
+      $prep->bindParam(':date_naissance', $date['date_naissance']);
+      $prep->bindParam(':date_inscription', $date['date_inscription']);
+      $prep->bindParam(':sex', $data['sex']);
+      $prep->bindParam(':email', $data['email']);
+      $prep->bindParam(':telephone', $data['telephone']);
+      $prep->bindParam(':access', 1);
+      $prep->bindParam(':adresse', $data['adresse']);
+      $prep->bindParam(':code_postale', $date['code_postale']);
+      $prep->bindParam(':ville', $date['ville']);
+
+      if ($prep->execute()) {
+        return $this->db->lastInsertId();
+      } else {
+        return false;
+      }
+
+  	}
+
+}
+
+?>
