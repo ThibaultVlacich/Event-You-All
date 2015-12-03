b1400dae37fccfd4a78554d533b43a8f1a224861
Thibault Vlacich <thibault.vlacich@gmail.com>
2015-12-03 11:16:50 +0100

    Improve login form CSS

--- apps/user/views/login.php
+++ apps/user/views/login.php
@@ -1,15 +1,17 @@
 <div class="app-user app-user-login">
   <h2>Connexion</h2>
-  <form id="Connexion" method="post" action="SUPERPAGEHTML.html">
-    <div>
-      <label for="email">Email</label><input name="email" id="email" type="text" placeholder="exemple@domaine.com" autofocus required /><br/>
-      <label for="password">Mot de Passe</label><input id="password" type="password" required /><br/>
-      <input type="submit" value="Se Connecter" />
+  <div class="login-form">
+    <form id="Connexion" method="post" action="<?php Config::get('config.base'); ?>/user/login">
+      <div>
+        <label for="email">Email</label><input name="email" id="email" type="text" required /><br/>
+        <label for="password">Mot de Passe</label><input id="password" type="password" required /><br/>
+        <input type="submit" value="Se Connecter" class="submit" />
+      </div>
+    </form>
+    <div class="links">
+      <a href="Pagemotdepasseoublié" class="password-lost">Mot de passe oublié</a>
+      <a href="Pagepasencoreinscrit" class="register">Pas encore inscrit ?</a>
+      <div class="clearer"></div>
     </div>
-  </form>
-  <div class="links">
-    <a href="Pagemotdepasseoublié" class="password-lost">Mot de passe oublié</a>
-    <a href="Pagepasencoreinscrit" class="register">Pas encore inscrit ?</a>
-    <div class="clearer"></div>
   </div>
 </div>
