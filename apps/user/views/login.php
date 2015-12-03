7884343e4e1df2ead9147daef88bdd66f074e816
Thibault Vlacich <thibault.vlacich@gmail.com>
2015-12-03 11:30:36 +0100

    Fix wrong links in login

--- apps/user/views/login.php
+++ apps/user/views/login.php
@@ -1,7 +1,7 @@
 <div class="app-user app-user-login">
   <h2>Connexion</h2>
   <div class="login-form">
-    <form id="Connexion" method="post" action="<?php Config::get('config.base'); ?>/user/login">
+    <form id="Connexion" method="post" action="<?php echo Config::get('config.base'); ?>/user/login">
       <div>
         <label for="email">Email</label><input name="email" id="email" type="text" required /><br/>
         <label for="password">Mot de Passe</label><input id="password" type="password" required /><br/>
@@ -9,9 +9,8 @@
       </div>
     </form>
     <div class="links">
-      <a href="Pagemotdepasseoublié" class="password-lost">Mot de passe oublié</a>
-      <a href="Pagepasencoreinscrit" class="register">Pas encore inscrit ?</a>
-      <div class="clearer"></div>
+      <a href="<?php echo Config::get('config.base'); ?>/user/register" class="password-lost">Mot de passe oublié</a>
+      <a href="<?php echo Config::get('config.base'); ?>/user/lostpassword" class="register">Pas encore inscrit ?</a>
     </div>
   </div>
 </div>
