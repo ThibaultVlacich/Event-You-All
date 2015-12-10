128dbfcd6320179d84f3d65c15f89517a514dccd
Thibault Vlacich <thibault.vlacich@gmail.com>
2015-12-10 10:52:37 +0100

    Add a default app for example

---
+++ apps/default/model.php
@@ -0,0 +1,22 @@
+<?php
+defined('EUA_VERSION') or die('Access denied');
+/**
+ * This is the Model for the app "NAME OF THE APP".
+ *
+ * @package apps/nameoftheapp
+ * @author Name of the author <author@isep.fr>
+ * @version 0.1.0-dev-dd-mm-yyyy
+ */
+
+// -> Need to replace here "Default" by the name of the app (capitalized)
+class DefaultModel {
+  protected $db;
+
+  public function __construct() {
+    $this->db = System::getDb();
+  }
+
+  // Then add methods (can be named whatever you want)
+}
+
+?>
