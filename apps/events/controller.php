e47211bb157af441147e44806cb8066ff5f63a4c
larbaretier <louis.arbaretier@gmail.com>
2015-12-11 09:05:35 +0100

    Modified requested data to create an event

--- apps/events/controller.php
+++ apps/events/controller.php
@@ -14,11 +14,11 @@ class EventsController extends Controller {
  function create_confirm() {
    $data = Request::getAssoc(array('nom','date_de','time_de',
    'date_fi','time_fi','nbpl','price','reg'
-   ,'adr','code_p','ville','pays','descript'
-   ,'bann','sujet','mclef','weborg'));
+   ,'adr','code_p','ville','pays','descript'));
    
 //modifier date...
    if (!in_array(null, $data, true)) {
+   	 $data+=Request::getAssoc(array('bann','sujet','mclef','weborg'));
 	 $date_debut=$data['date_de'].$data['time_de'];
 	 $date_fin=$data['date_fi'].$data['time_fi'];
      $this->model->createEvent($data);
