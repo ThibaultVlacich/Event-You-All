b33e086b8371b0c39ac6730401a84f62ab785d6b
Thibault Vlacich <thibault.vlacich@gmail.com>
2015-12-13 11:23:11 +0100

    Use strftime() instead of date()
    For localized dates

--- apps/events/controller.php
+++ apps/events/controller.php
@@ -25,8 +25,8 @@ class EventsController extends Controller {
 
      if (!empty($data['date_debut']) && $data['date_debut'] != '0000-00-00 00:00:00') {
        $date_debut_timestamp = strtotime($data['date_debut']);
-       $data['date_debut'] = date('D. d F Y', $date_debut_timestamp);
-       $data['heure_debut'] = date('H:i', $date_debut_timestamp);
+       $data['date_debut'] = strftime('%a. %d %b. %Y', $date_debut_timestamp);
+       $data['heure_debut'] = strftime('%H:%M', $date_debut_timestamp);
      } else {
        $data['date_debut'] = null;
        $data['heure_debut'] = null;
@@ -34,8 +34,8 @@ class EventsController extends Controller {
 
      if (!empty($data['date_fin']) && $data['date_fin'] != '0000-00-00 00:00:00') {
        $date_fin_timestamp = strtotime($data['date_fin']);
-       $data['date_fin'] = date('D. d F Y', $date_fin_timestamp);
-       $data['heure_fin'] = date('H:i', $date_fin_timestamp);
+       $data['date_fin'] = strftime('%a. %d %b. %Y', $date_fin_timestamp);
+       $data['heure_fin'] = strftime('%H:%M', $date_fin_timestamp);
      } else {
        $data['date_fin'] = null;
        $data['heure_fin'] = null;
