281979d762f97c1ef1f9fc62915de1c88c23aa4b
leoplouvier <plouvierleo@gmail.com>
2015-12-15 11:50:14 +0100

    forum controller update

--- apps/forum/controller.php
+++ apps/forum/controller.php
@@ -7,7 +7,43 @@ class ForumController extends Controller {
  function detail() {
 
  }
- function Forum(){}
+ function Forum(array $params){
+   if (isset($params[0])) {
+     $topic_id = intval($params[0]);
+
+     // Récupérer l'evenement lié depuis le model
+     if (!($data = $this->model->getTopic($topic_id))) {
+       return array();
+     }
+
+     if (!empty($data['date_creation']) && $data['date_creation'] != '0000-00-00 00:00:00') {
+       $date_debut_timestamp = strtotime($data['date_creation']);
+       $data['date_creation'] = strftime('%a. %d %b. %Y', $date_debut_timestamp);
+     } else {
+       $data['date_creation'] = null;
+     }
+     // Retourner les infos récupérées
+     return $data;
+   }
+ }
+
+ function create() {
+
+ }
+
+ function create_confirm() {
+
+   $data = Request::getAssoc(array('sujet','administrateur','evenement','date_creation'));
+
+   if (!in_array(null, $data, true)) {
+     $date_creation = $data['date_creation'];
+
+     $data['date_creation'] = $date_creation;
+
+
+     $this->model->createEvent($data);
+   }
+ }
 
 }
 
