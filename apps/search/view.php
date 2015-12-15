4f9a0d2f5fb6fe8b7d4d7026fbd895e8f3ac8355
HugoMichard <hugomichard@yahoo.com>
2015-12-15 11:58:06 +0100

    add search app

---
+++ apps/search/view.php
@@ -0,0 +1,30 @@
+<?php
+defined('EUA_VERSION') or die('Access denied');
+/**
+ * This is the View for the app "NAME OF THE APP".
+ *
+ * @package apps/nameoftheapp
+ * @author Name of the author <author@isep.fr>
+ * @version 0.1.0-dev-dd-mm-yyyy
+ */
+
+// This file is not needed
+class SearchView extends View {
+  function __construct() {
+    // Here assign CSSs/JSs for all the app
+    $this->assign('css',Config::get('config.base').'/apps/search/styles/style.css');
+  }
+
+  // Method for the module
+  public function search() {
+    // Here assign CSSs/JSs for that method
+    // EX : $this->assign('css', Config::get('config.base').'/apps/user/styles/style.css');
+    
+    // The assign the view for the module
+    $this->setTemplate('/apps/NAMEOFTHEAPP/views/VIEW.php');
+  }
+
+
+}
+
+?>
