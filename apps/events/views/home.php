a9b2e13a2271b244a995303fbca7dd2bb9771952
Thibault Vlacich <thibault.vlacich@gmail.com>
2015-12-16 16:43:57 +0100

    [#1] Home page

--- apps/events/views/home.php
+++ apps/events/views/home.php
@@ -10,15 +10,16 @@
           <a href="<?php echo Config::get('config.base'); ?>/events/detail/<?php echo $event['id']; ?>"><img src="<?php echo $event['poster']; ?>" alt=""></a>
         </div>
         <div class="caption">
+          <span class="theme">Concert</span>
           <h2><a href="<?php echo Config::get('config.base'); ?>/events/detail/<?php echo $event['id']; ?>"><?php echo $event['nom']; ?></a></h2>
           <?php
             if (( !empty($event['date_debut']) && empty($event['date_fin']) ) || ( !empty($event['date_debut']) && !empty($event['date_fin']) && $event['date_debut'] == $event['date_fin'] )) {
           ?>
-            <div class="date">Le <?php echo $event['date_debut']; ?></div>
+          <div class="date">Le <?php echo $event['date_debut']; ?></div>
           <?php
             } else if ( !empty($event['date_debut']) && !empty($event['date_fin']) && $event['date_debut'] != $event['date_fin'] ) {
           ?>
-            <div class="date">Du <?php echo $event['date_debut']; ?> au <?php echo $event['date_fin']; ?></div>
+          <div class="date">Du <?php echo $event['date_debut']; ?> au <?php echo $event['date_fin']; ?></div>
           <?php } ?>
         </div>
       </li>
