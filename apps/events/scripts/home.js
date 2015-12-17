9df5191c8a9111243b94686d466746c0ad8e8aed
Thibault Vlacich <thibault.vlacich@gmail.com>
2015-12-17 16:36:42 +0100

    Revert "[#1] Fix buttons not showing on home slideshow"
    This reverts commit 369fe07397930258f9a59e6a912f798337f1fc25.

--- apps/events/scripts/home.js
+++ apps/events/scripts/home.js
@@ -3,12 +3,7 @@ $(function() {
     width: '100%',
     height: 600,
     play: {
-      // Autoplay
       auto: true
-    },
-    navigation: {
-      // Deactivate default navigation buttons, we are using our ones
-      active: false
     }
   });
 });
