369fe07397930258f9a59e6a912f798337f1fc25
Thibault Vlacich <thibault.vlacich@gmail.com>
2015-12-17 15:51:04 +0100

    [#1] Fix buttons not showing on home slideshow

--- apps/events/scripts/home.js
+++ apps/events/scripts/home.js
@@ -3,7 +3,12 @@ $(function() {
     width: '100%',
     height: 600,
     play: {
+      // Autoplay
       auto: true
+    },
+    navigation: {
+      // Deactivate default navigation buttons, we are using our ones
+      active: false
     }
   });
 });
