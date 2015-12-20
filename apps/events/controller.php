c7204f572f7800d4d00b26b4e4e9f20dbd627ab9
larbaretier <louis.arbaretier@gmail.com>
2015-12-20 22:25:55 +0100

    [#10] Started making modify event view, controller and model page
    View almost done : just need to put date/time and all the scrolling
    menus to the default choice
    Controller : confirmation still needs some work
    Model : almost finished, just need to add date and banner/poster

--- apps/events/controller.php
+++ apps/events/controller.php
@@ -184,5 +184,136 @@ class EventsController extends Controller {
 
     return $events;
   }
+  
+  
+  function modif (array $params) {
+    if (isset($params[0])) {
+      $event_id = intval($params[0]);
+
+      // Récupérer l'evenement lié depuis le model
+      if (!($data = $this->model->getEvent($event_id))) {
+        return array();
+      }
+
+      if (!empty($data['date_debut']) && $data['date_debut'] != '0000-00-00 00:00:00') {
+        $date_debut_timestamp = strtotime($data['date_debut']);
+        $data['date_debut'] = strftime('%a. %d %b. %Y', $date_debut_timestamp);
+        $data['heure_debut'] = strftime('%H:%M', $date_debut_timestamp);
+      } else {
+        $data['date_debut'] = null;
+        $data['heure_debut'] = null;
+      }
+
+      if (!empty($data['date_fin']) && $data['date_fin'] != '0000-00-00 00:00:00') {
+        $date_fin_timestamp = strtotime($data['date_fin']);
+        $data['date_fin'] = strftime('%a. %d %b. %Y', $date_fin_timestamp);
+        $data['heure_fin'] = strftime('%H:%M', $date_fin_timestamp);
+      } else {
+        $data['date_fin'] = null;
+        $data['heure_fin'] = null;
+      }
+    // Get creator's name and id
+    $data['creator'] = $this->model->getCreatorForEvent($data['id']);
+      // Get user's id
+
+      // Retourner les infos récupérées
+      //print_r ($data);
+
+      return $data;
+    }
+    else{echo false;}
+  }
 }
+
+
+
+  function modif_confirm() {
+    $data = Request::getAssoc(array('nom','date_de','time_de','date_fi','time_fi','nbpl','price','reg','adr','code_p','ville','pays','descript','theme','type'));
+    echo '101 dalm';
+    $errors = array();
+    
+    if (!in_array(null, $data, true)) {
+      $data += Request::getAssoc(array('sujet','mclef','weborg','priv'));
+
+      $maxwidth = 100000;
+      $minwidth = 0;
+      $maxheight = 100000;
+      $minheight = 0;
+      $banner = Request::get('bann', null, 'FILES');
+      $message_erreur = '';
+
+      if(!empty($banner['name'])) {
+        if(!$banner['error']) {
+            $extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
+            $extension_upload = strtolower(  substr(  strrchr($banner['name'], '.')  ,1)  );
+            if ( in_array($extension_upload,$extensions_valides) ){
+                $sizeimage=getimagesize($banner['tmp_name']);
+                if ($sizeimage[0] > $minwidth and $sizeimage[1] > $minheight){
+                    $new_file_name = $banner['name'];
+
+                    move_uploaded_file($banner['tmp_name'], UPLOAD_DIR.'events'.DS.'banner'.DS.$new_file_name);
+
+                    $data['bann'] = Config::get('config.base').'/upload/events/banner/'.$new_file_name;
+                } else {
+                    $errors += array('Problème de dimension pour la bannière : trop petit en hauteur et/ou en largeur');
+                }
+            } else {
+             $errors += array('Problème d\'extension pour la bannière : votre fichier n\'est pas du type png, jpeg, jpg ou gif');
+            }
+        } else{
+            $errors += array('Problème de Serveur');
+        }
+      }
+
+      $poster = Request::get('poster', null, 'FILES');
+
+
+      if(!empty($poster['name'])) {
+        if(!$poster['error']) {
+            $extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
+            $extension_upload = strtolower(  substr(  strrchr($poster['name'], '.')  ,1)  );
+            if ( in_array($extension_upload,$extensions_valides) ){
+                $sizeimage=getimagesize($poster['tmp_name']);
+                if ($sizeimage[0] > $minwidth and $sizeimage[1] > $minheight){
+                    $new_file_name = $poster['name'];
+
+                    move_uploaded_file($poster['tmp_name'], UPLOAD_DIR.'events'.DS.'poster'.DS.$new_file_name);
+
+                    $data['poster'] = Config::get('config.base').'/upload/events/poster/'.$new_file_name;
+                } else {
+                    $errors += array('Problème de dimension pour le poster : trop petit en hauteur et/ou en largeur');
+                }
+            } else {
+             $errors += array('Problème d\'extension pour le poster : votre fichier n\'est pas du type png, jpeg, jpg ou gif');
+            }
+        } else{
+            $errors += array('Problème de Serveur');
+        }
+      }
+
+
+      $date_debut = $data['date_de'].' '.$data['time_de'];
+      $date_fin = $data['date_fi'].' '.$data['time_fi'];
+
+      $data['priv'] = false;
+
+      if (!empty($data['priv'])) {
+        $data['priv'] = true;
+      }
+/*
+	    if (empty($data['bann'])) {
+        $data['bann'] = '';
+      }
+
+      if (empty($data['mclef'])) {
+        $data['mclef'] = '';
+      }*/
+
+      $data['date_de'] = $date_debut;
+      $data['date_fi'] = $date_fin;
+      $id_event = $this->model->modifEvent($data);
+
+	    return array('id' => $id_event, 'error' => $errors);
+    }
+  }
 ?>
---
+++ apps/events/views/modifevent.php
@@ -0,0 +1,287 @@
+        <section class="blocinscri">
+        <?php
+            if (empty($model)) {
+              // No event based on asked id
+          ?>
+          <div class="note error">
+            <i class="fa fa-exclamation-triangle"></i>
+            <ul>
+              <li>L'événement demandé n'existe pas !</li>
+            </ul>
+          </div>
+          <?php
+              return;
+            }?>
+            <?php 
+            $session = System::getSession();
+            if($session->isConnected()) {
+              // User is logged in
+              $user_id = $_SESSION['userid'];
+
+              if ($model['creator']['id'] == $user_id) {
+                // User is the creator?>
+            <form method='post' action='<?php echo Config::get('config.base'); ?>/events/modif_confirm/<?php echo $model['id']; ?>' enctype="multipart/form-data">
+                <h2>Modifier l'événement</h2>
+              <div class="create_part" class="r1">
+                <h3>1-A propos de l'événement</h3>
+
+                <div class="centre">
+
+                        <div class="label">
+                            <label for='nom'>Nom<span class="required">*</span>  </label>
+                        </div>
+                    <div class="fill">
+                        <input type='text' name='nom' id='nom'  required placeholder="ex : Mon evenement" value="<?php echo $model['nom']?>">
+                    </div>
+                        <div class="label">
+                        <label for='mail'>Theme<span class="required">*</span>  </label>
+                        </div>
+                    <div class="fill">
+                        <select  name='theme' id='theme' selected="selected" value=3>
+                            <optgroup label='Musique'/>
+                                <option value=1>Classique</option>
+                                <option value=2>Metal</option>
+                                <option value=3>Rock</option>
+                                <option value=4>Autres</option>
+                            <optgroup label='Cinema'/>
+                                <option value=5>Action</option>
+                                <option value=6>Thriller</option>
+                                <option value=7>Familial</option>
+                                <option value=8>Comedie</option>
+                                <option value=9>Autres</option>
+                            <optgroup label='Image'/>
+                                <option value=10>Peinture</option>
+                                <option value=11>Photographie</option>
+                                <option value=12>Autres</option>
+                        </select>
+                    </div>
+                         <div class="label">
+                        <label for='type'>Type<span class="required">*</span>  </label>
+                         </div>
+                    <div class="fill">
+                        <select  name='type' id='type'>
+                                <option value=1>Exposition</option>
+                                <option value=2>Projection</option>
+                                <option value=3>Conference</option>
+                                <option value=4>Concert</option>
+                                <option value=5>Projection</option>
+                                <option value=5>Autres</option>
+                        </select>
+                    </div>
+                    <br>
+                    <!--les champs ne marchent pas pour firefox d'où le placeholder...-->
+                         <div class="label">
+                        <label for='date_de'>Début<span class="required">*</span>  </label>
+                         </div>
+                    <div class="fill">
+                        <input type="date" name="date_de" id="date_de" required placeholder="dd/mm/yyyy">
+                        <input type="time" name="time_de" id="time_de" required placeholder="hh/mm">
+                    </div>
+                         <div class="label">
+                        <label for='date_fi'>Fin<span class="required">*</span>  </label>
+                         </div>
+                    <div class="fill">
+                        <input type="date" name="date_fi" id="date_fi" required placeholder="dd/mm/yyyy">
+                        <input type="time" name="time_fi" id="time_fi" required placeholder="hh/mm">
+                    </div>
+                    <br>
+                         <div class="label">
+                        <label for='nbpl'>Capacité<span class="required">*</span>  </label>
+                         </div>
+                    <div class="fill">
+                        <input type="number" name="nbpl" id="nbpl" required min="2" placeholder="10" value="<?php echo $model['capacite']?>">
+                    </div>
+                         <div class="label">
+                        <label for='price'>Prix (euros)<span class="required">*</span>  </label>
+                         </div>
+                    <div class="fill">
+                        <input type="number" name="price" id="price" required min="0" placeholder="0" value="<?php echo $model['prix']?>">
+
+                    </div>
+                        <div class="label">
+                            <label for='mclef'>Mots clefs  </label>
+                        </div>
+                    <div class="fill">
+                        <input type='text' name='mclef' id='mclef' maxlength="100" placeholder="motclef1, motclef2, motclef3..." value="<?php echo $model['mot_clef']?>">
+                    </div>
+                    <br>
+                    <p><input type='checkbox' name='priv' id='priv' <?php if ($model['prive']!=0){echo "checked";}?>/>
+
+                     <label for='priv'>Privé</label></p>
+                </div>
+                <br>
+                </div>
+                <div class="create_part2">
+                    <h3>2-Paramètres d'inscription (si événement privé)</h3>
+                <div class="centre">
+
+                        <div class="label">
+                            <label for='gpadm'>Groupes admis  </label>
+                        </div>
+                    <div class="fill">
+                        <input type='text' name='gpadm' id='gpadm'  placeholder="groupe1, groupe2, groupe3...">
+                    </div>
+
+                        <div class="label">
+                            <label for='padm'>Personnes admises  </label>
+                        </div>
+                    <div class="fill">
+                        <input type='text' name='padm' id='padm'  placeholder="personne1, personne2, personne3...">
+                    </div>
+                </div>
+                <br>
+                </div>
+                 <div class="create_part">
+                    <h3>3-Black list</h3>
+                <div class="centre">
+
+                        <div class="label">
+                            <label for='blist'>Personnes non admises  </label>
+                        </div>
+                    <div class="fill">
+                        <input type='text' name='blist' id='blist' placeholder="personne1, personne2, personne3...">
+                    </div>
+                </div>
+                <br>
+                 </div>
+                 <div class="create_part2">
+                    <h3>4-Organisateur</h3>
+                <div class="centre">
+
+                       
+                        <div class="label">
+                                <label for='partn'>Partenaires et Sponsors </label>
+                        </div>
+                    <div class="fill">
+                    <!-- TODO change partenaire -->
+                        <input type='text' name='partn' id='partn' placeholder="partenaire1, partenaire2, sponsor1..." value="<?php echo $model['nom']?>">
+                    </div>
+
+                        <div class="label">
+                                <label for='weborg'>Site internet </label>
+                        </div>
+                    <div class="fill">
+                        <input type='text' name='weborg' id='weborg' placeholder="wwww.exemple.com" value="<?php echo $model['site_web']?>">
+                    </div>
+                </div>
+                <br>
+                 </div>
+                 <div class="create_part">
+                    <h3>5-Lieu</h3>
+                <div class="centre">
+                         <div class="label">
+                        <label for='reg'>Région<span class="required">*</span>  </label>
+                         </div>
+                    <div class="fill">
+                        <select  name='reg' id='reg'>
+                                <option value='r1'>R1</option>
+                                <option value='r2'>R2</option>
+                        </select>
+                    </div>
+
+                        <div class="label">
+                                <label for='adr'>Adresse<span class="required">*</span>  </label>
+                        </div>
+                    <div class="fill">
+                        <input type='text' name='adr' id='adr' required placeholder="14 rue event" value="<?php echo $model['adresse']?>">
+                    </div>
+                         <div class="label">
+                        <label for='code_p'>Code postal<span class="required">*</span>  </label>
+                         </div>
+                    <div class="fill">
+                        <input type="text" name="code_p" id="code_p" required minlength=5 maxlength="5" placeholder="00000" value="<?php echo $model['code_postal']?>">
+                    </div>
+                         <div class="label">
+                        <label for='ville'>Ville<span class="required">*</span>  </label>
+                         </div>
+                    <div class="fill">
+                        <input type="text" name="ville" id="ville" required placeholder="Paris" value="<?php echo $model['ville']?>">
+                    </div>
+                         <div class="label">
+                        <label for='pays'>Pays<span class="required">*</span>  </label>
+                         </div>
+                    <div class="fill">
+                        <select  name='pays' id='reg'>
+                                <option value='fr'>France</option>
+                                <option value='cn'>Canada</option>
+                        </select>
+                    </div>
+                </div>
+                <br>
+                 </div>
+                 <div class="create_part2">
+                    <h3>6-Page de l'événement</h3>
+                <div class="centre">
+
+
+
+                            <label for='descript'>Description  </label>
+
+
+
+
+                            <br>
+
+                        <textarea required name="descript" rows=10 cols=100 ><?php echo $model['description']?></textarea>
+
+                    <br>
+
+                            <label for='bann'>Bannière  </label>
+
+
+                        <input type="file" id="bann" name="bann">
+                        <p class='gauche'>Dimensions : 400px*900px</p>
+
+                        <!--mettre les vrais dimensions-->
+                        
+                        <label for='poster'>Poster  </label>
+
+
+                        <input type="file" id="poster" name="poster">
+                        <p class='gauche'>Dimensions : 400px*900px</p>
+
+                        <!--mettre les vrais dimensions-->
+
+                </div>
+                <br>
+                </div>
+                 <div class="create_part">
+                    <h3>7-Invitation à l'événement</h3>
+                <div class="centre">
+
+                        <div class="label">
+                            <label for='invitm'>Envoyer un e-mail d'invitation à  </label>
+                        </div>
+                        <br>
+                        <textarea name="invitm" rows=10 cols=100>invite@gmail.com, invite3@gmail.com ...</textarea>
+
+                </div>
+                 </div>
+                 <p><input type='checkbox' name='condi' id='condi' required/>
+                     <label for='condi'>J'accepte les conditions d'utilisation du site... <span class="required">*</span></label></p>
+                 <br>
+                 <p class="gauche"><span class="required">*</span> : champs obligatoires</p>
+                 <!--Mettre différentes fonctions à chaque bouton -->
+                 <p><input id="bouton" type='submit' value='Envoyer'/>
+                    <input id="bouton" type='submit' value='Enregistrer sans publier'/>
+                    <input id="bouton" type='submit' value='Annuler'/></p>
+            </form>
+              <?php }
+              else{ // not creator?>
+              <div class="note error">
+            <i class="fa fa-exclamation-triangle"></i>
+            <ul>
+              <li>L'événement demandé n'est pas le votre</li>
+            </ul>
+          </div>
+              <?php }
+            } 
+            else{//not connected?>
+            <div class="note error">
+            <i class="fa fa-exclamation-triangle"></i>
+            <ul>
+              <li>Vous devez être connecté</li>
+            </ul>
+          </div>
+            <?php } ?>
+        </section>
