5d115dc1651fdadd5bcb2dde23806074864289ae
Thibault Vlacich <thibault.vlacich@gmail.com>
2016-01-09 19:20:30 +0100

    [#19] Add a "create an event" button on home page

--- apps/events/views/home.php
+++ apps/events/views/home.php
@@ -31,43 +31,47 @@
   </div>
   <?php } ?>
   <div class="home-wrapper">
-    <h2 class="title">Faire une recherche</h2>
-    <form action="<?php echo Config::get('config.base'); ?>/search" method="get">
-      <div class="search-form">
-        <select name="region">
-          <option disabled selected hidden>Région</option>
-          <?php foreach($model['regions'] as $region) { ?><option value="<?php echo $region['id']; ?>"><?php echo $region['nom']; ?></option><?php } ?>
-        </select>
-        <select name="theme">
-          <option disabled selected hidden>Thème</option>
-          <?php foreach($model['themes'] as $theme) { ?><option value="<?php echo $theme['id']; ?>"><?php echo $theme['nom']; ?></option><?php } ?>
-        </select>
-        <input type="date" name="date" placeholder="Date">
-        <input type="submit" value="Rechercher">
-      </div>
-    </form>
-    <h2 class="title">Les événements à venir</h2>
-    <ul class="events">
-      <?php foreach($model['events'] as $event) { ?>
-        <li>
-          <div class="poster">
-            <a href="<?php echo Config::get('config.base'); ?>/events/detail/<?php echo $event['id']; ?>"><img src="<?php echo $event['poster']; ?>" alt=""></a>
-          </div>
-          <div class="caption">
-            <?php if(isset($event['theme'])) { ?><span class="theme"><?php echo $event['theme']['nom']; ?></span><?php } ?>
-            <h3><a href="<?php echo Config::get('config.base'); ?>/events/detail/<?php echo $event['id']; ?>"><?php echo $event['nom']; ?></a></h3>
-            <?php
-              if (( !empty($event['date_debut']) && empty($event['date_fin']) ) || ( !empty($event['date_debut']) && !empty($event['date_fin']) && $event['date_debut'] == $event['date_fin'] )) {
-            ?>
-            <div class="date">Le <?php echo $event['date_debut']; ?></div>
-            <?php
-              } else if ( !empty($event['date_debut']) && !empty($event['date_fin']) && $event['date_debut'] != $event['date_fin'] ) {
-            ?>
-            <div class="date">Du <?php echo $event['date_debut']; ?> au <?php echo $event['date_fin']; ?></div>
-            <?php } ?>
-          </div>
-        </li>
-      <?php } ?>
-    </ul>
+    <section>
+      <h2 class="title">Faire une recherche</h2>
+      <form action="<?php echo Config::get('config.base'); ?>/search" method="get">
+        <div class="search-form">
+          <select name="region">
+            <option disabled selected hidden>Région</option>
+            <?php foreach($model['regions'] as $region) { ?><option value="<?php echo $region['id']; ?>"><?php echo $region['nom']; ?></option><?php } ?>
+          </select>
+          <select name="theme">
+            <option disabled selected hidden>Thème</option>
+            <?php foreach($model['themes'] as $theme) { ?><option value="<?php echo $theme['id']; ?>"><?php echo $theme['nom']; ?></option><?php } ?>
+          </select>
+          <input type="date" name="date" placeholder="Date">
+          <input type="submit" value="Rechercher">
+        </div>
+      </form>
+    </section>
+    <section>
+      <h2 class="title">Les événements à venir <a href="<?php echo Config::get('config.base'); ?>/events/create" class="create"><i class="fa fa-plus"></i> Ajouter un événement</a></h2>
+      <ul class="events">
+        <?php foreach($model['events'] as $event) { ?>
+          <li>
+            <div class="poster">
+              <a href="<?php echo Config::get('config.base'); ?>/events/detail/<?php echo $event['id']; ?>"><img src="<?php echo $event['poster']; ?>" alt=""></a>
+            </div>
+            <div class="caption">
+              <?php if(isset($event['theme'])) { ?><span class="theme"><?php echo $event['theme']['nom']; ?></span><?php } ?>
+              <h3><a href="<?php echo Config::get('config.base'); ?>/events/detail/<?php echo $event['id']; ?>"><?php echo $event['nom']; ?></a></h3>
+              <?php
+                if (( !empty($event['date_debut']) && empty($event['date_fin']) ) || ( !empty($event['date_debut']) && !empty($event['date_fin']) && $event['date_debut'] == $event['date_fin'] )) {
+              ?>
+              <div class="date">Le <?php echo $event['date_debut']; ?></div>
+              <?php
+                } else if ( !empty($event['date_debut']) && !empty($event['date_fin']) && $event['date_debut'] != $event['date_fin'] ) {
+              ?>
+              <div class="date">Du <?php echo $event['date_debut']; ?> au <?php echo $event['date_fin']; ?></div>
+              <?php } ?>
+            </div>
+          </li>
+        <?php } ?>
+      </ul>
+    </section>
   </div>
 </div>
