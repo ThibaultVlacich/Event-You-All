0b8b4d9071237322884bc9ee99188e55091652b0
Thibault Vlacich <thibault.vlacich@gmail.com>
2016-01-10 01:59:56 +0100

    Improve again the create/modif form

--- apps/events/scripts/create.js
+++ apps/events/scripts/create.js
@@ -1,3 +1,26 @@
 $(function() {
   CKEDITOR.replace('description');
+
+  /**
+   * Make the sidebar sticky
+  */
+  var $sticky = $('.sticky');
+
+  if (!!$sticky.offset()) { // Execute all this only if we have an element with the .sticky class
+
+    var stickyTop = $sticky.offset().top; // Top offset position of the element
+
+    // Fix its width to avoid a bug
+    $sticky.css('width', $sticky.width());
+
+    $(window).scroll(function() {
+      var windowTop = $(window).scrollTop();
+
+      if (stickyTop < windowTop){
+        $sticky.css({ position: 'fixed', top: 0 });
+      } else {
+        $sticky.css('position','static');
+      }
+    });
+  }
 });
