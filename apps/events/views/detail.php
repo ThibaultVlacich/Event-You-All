cbd0943240b843c7976009c57665e61d7052e250
Thibault Vlacich <thibault@Thibaults-MacBook-Pro.local>
2016-01-11 15:50:30 +0100

    [#8] Add the rate of the event on detail page

--- apps/events/views/detail.php
+++ apps/events/views/detail.php
@@ -61,6 +61,7 @@
             </li>
             <?php if ($model['prix'] > 0) { ?><li><i class="fa fa-li fa-money"></i> <?php echo $model['prix']; ?>&nbsp;€</li><?php } ?>
             <?php if ($model['capacite'] > 0) { ?><li><i class="fa fa-li fa-users"></i> <?php echo $model['capacite']; ?> places</li><?php } ?>
+            <?php if ($model['rate']) { ?><li><i class="fa fa-li fa-star"></i> Note de l'événement : <?php echo round($model['rate']); ?>/5</li><?php } ?>
           </ul>
           <?php if(!empty($model['description'])) { ?>
           <div class="description">
