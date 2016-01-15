c13badc56f4ad44c85800fbc1c49db92c3df3031
alexpoulpe <alexandrebgay@gmail.com>
2016-01-15 15:21:58 +0100

    [#8]legal et blabla

--- apps/about/views/about.php
+++ apps/about/views/about.php
@@ -3,5 +3,5 @@
 
             <h2>A propos d' Event-You-All</h2>
 
-        <p id="présentation">Considérée comme la plus grande communauté mondiale d'amateur d'arts event-you-all est un site non lucratif regroupant des evenements artistiques (concert, exposition etc...) dans le monde entier afin de réunir des gens ayant une passion commune pour le plus grand plaisir des petits et des grands </p>
+        <p id="présentation">Considérée comme la plus grande communauté mondiale d'amateur d'arts Event-You-All est un site non lucratif regroupant des evenements artistiques (concerts, expositions, etc...) dans le monde entier afin de réunir des gens ayant une passion commune pour le plus grand plaisir des petits et des grands. </p>
         <img class='event' src="<?php echo Config::get('config.base')?>/apps/about/Image/logo.png" alt="Logo Event-You-All" />
--- apps/legal/views/legal.php
+++ apps/legal/views/legal.php
@@ -3,16 +3,16 @@
         <div id="bandeau">
             <h2>Mentions légales</h2>
         </div>
-        <p id="présentation">Considérée comme la plus grande communauté mondiale d'ammateur d'arts event-you-all est un site non lucratif regroupant des evenements artistiques (concert, exposition etc...) dans le monde entier afin de réunir des gens ayant une passion communes pour le pus grand plaisir des petits et des grands </p>
+        <p id="présentation">Considérée comme la plus grande communauté mondiale d'amateur d'arts Event-You-All est un site non lucratif regroupant des evenements artistiques (concerts, expositions etc...) dans le monde entier afin de réunir des gens ayant une passion commune pour le pus grand plaisir des petits et des grands. </p>
         <p>Nom du site : Event-You-All </p>
         <p>Url du site : event-you-all.thibault.vlacich.fr</p>
-        <p>Propriétaire du site : </p>
+        <p>Propriétaire du site : Mohammed Sellami</p>
         <p>Adresse postale : 28 Rue Notre Dame des Champs, 75006 Paris </p>
-        <p>Nom du créateur du site : </p>
-        <p>Url du créateur du site : </p>
-        <p>Nom du responsable de la publication : </p>
+        <p>Nom du créateur du site : Web'Design </p>
+        <p>Url du créateur du site : isep.fr </p>
+        <p>Nom du responsable de la publication : Web'Design</p>
         <p>Nom du Webmaster : </p>
-        <p>Contact email : <?php echo $mail ?> </p>
-        <p>Hébergeur : </p>
-        <p>Adresse postale hébergeur : </p>
-        <p>Site déclaré à la CNIL : </p>
+        <p>Contact email : <?php echo Config::get('config.email') ?> </p>
+        <p>Hébergeur : OVH </p>
+        <p>Adresse postale hébergeur :  2 rue Kellermann – BP 80157 59053 ROUBAIX CEDEX 1</p>
+        <p>Site déclaré à la CNIL : Non </p>
