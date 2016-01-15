d6937bf4b6d1395bef9e8bec93ad3c0330c0ec65
alexpoulpe <alexandrebgay@gmail.com>
2016-01-15 13:54:25 +0100

    [#8] faq delete

--- apps/faq/admin/views/getFaq.php
+++ apps/faq/admin/views/getFaq.php
@@ -8,9 +8,11 @@
       <p class="question"> <?php echo $qw['question']; ?></p>
       <p class="reponse"><?php echo $qw['reponse']; ?></p>
       <a href="<?php echo Config::get('config.base'); ?>/admin/faq/modify/<?php echo $qw['id'];?>">Modifier</a>
+      <a href="<?php echo Config::get('config.base'); ?>/admin/faq/delete/<?php echo $qw['id'];?>">Supprimer</a>
   </section>
 
 
 <?php } ?>
 <a href="<?php echo Config::get('config.base'); ?>/admin/faq/add">Ajouter</a>
+
 </div>
