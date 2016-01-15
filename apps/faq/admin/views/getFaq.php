bc7ac66becd27321ff92ab485d9115206fa44fce
alexpoulpe <alexandrebgay@gmail.com>
2016-01-15 15:03:36 +0100

    [#8]RAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- apps/faq/admin/views/getFaq.php
+++ apps/faq/admin/views/getFaq.php
@@ -7,12 +7,12 @@
   <section class="QW">
       <p class="question"> <?php echo $qw['question']; ?></p>
       <p class="reponse"><?php echo $qw['reponse']; ?></p>
-      <a href="<?php echo Config::get('config.base'); ?>/admin/faq/modify/<?php echo $qw['id'];?>">Modifier</a>
-      <a href="<?php echo Config::get('config.base'); ?>/admin/faq/delete/<?php echo $qw['id'];?>">Supprimer</a>
+      <a class="modifybutton" href="<?php echo Config::get('config.base'); ?>/admin/faq/modify/<?php echo $qw['id'];?>">Modifier</a>
+      <a class="deletebutton" href="<?php echo Config::get('config.base'); ?>/admin/faq/delete/<?php echo $qw['id'];?>">Supprimer</a>
   </section>
 
 
 <?php } ?>
-<a href="<?php echo Config::get('config.base'); ?>/admin/faq/add">Ajouter</a>
+<a class="addbutton" href="<?php echo Config::get('config.base'); ?>/admin/faq/add">Ajouter</a>
 
 </div>
