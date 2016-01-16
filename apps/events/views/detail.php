89db4f51f18bd59a9bcf7ab8df81bf8c565a1a65
larbaretier <louis.arbaretier@gmail.com>
2016-01-16 15:28:28 +0100

    [#44] made website appear

--- apps/events/views/detail.php
+++ apps/events/views/detail.php
@@ -99,6 +99,7 @@
             </li>
             <?php if ($model['prix'] > 0) { ?><li><i class="fa fa-li fa-money"></i> <?php echo $model['prix']; ?>&nbsp;€</li><?php } ?>
             <?php if ($model['capacite'] > 0) { ?><li><i class="fa fa-li fa-users"></i> <?php echo $model['capacite']; ?> places</li><?php } ?>
+            <?php if (isset($model['site_web'])) { ?><li><i ></i> <a href="<?php echo $model['site_web']; ?>">Site internet</a></li><?php } ?>
             <?php if ($model['rate']) { ?><li><i class="fa fa-li fa-star"></i> Note de l'événement : <?php echo round($model['rate']); ?>/5</li><?php } ?>
           </ul>
           <?php if(!empty($model['description'])) { ?>
