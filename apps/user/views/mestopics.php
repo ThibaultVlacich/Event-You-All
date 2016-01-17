cf86a74bd96ec84ff942137d6b36111d82f15634
larbaretier <louis.arbaretier@gmail.com>
2016-01-17 15:12:09 +0100

    My topics relook

--- apps/user/views/mestopics.php
+++ apps/user/views/mestopics.php
@@ -3,24 +3,24 @@
 <a href='<?php echo Config::get('config.base'); ?>/user/myprofil' class='boutoncompte'>Retourner sur mon profil</a>
 <a href='<?php echo Config::get('config.base'); ?>/forum' class='boutoncompte'>Accès au Forum</a>
 <h2  class='voirmestopics'>Les Topics que j'ai créé</h2>
-      <ul class="topic">
-        <li>Titre</li>
-        <li>Description</li>
-        <li>Date de création</li>
-      </ul>
+<table>
+      <tr class="topic">
+        <th>Titre</th>
+        <th>Description</th>
+        <th>Date de création</th>
+      </tr>
         <?php $i=1; ?>
         <?php foreach($model['topicscreation'] as $topic) { ?>
-        <div class="topicdiv">
-          <ul class=<?php if ($i%2==0){echo"detailtopic1" ;} else{echo "detailtopic2"; } ?>>
-          <li class="titre">
+          <tr class=<?php if ($i%2==0){echo"detailtopic1" ;} else{echo "detailtopic2"; } ?>>
+          <th class="titre">
               <a href="<?php echo Config::get('config.base'); ?>/forum/Topic/<?php echo $topic['id']; ?>"><?php echo $topic['titre']; ?></a>
-          </li>
-          <li class="description">
-              <a href="<?php echo Config::get('config.base'); ?>/forum/Topic/<?php echo $topic['id']; ?>"><?php echo $topic['description']; ?></a>
-          </li>
-          <li class="date">
+          </th>
+          <th class="description">
+              <a href="<?php echo Config::get('config.base'); ?>/forum/Topic/<?php echo $topic['id']; ?>"><?php echo substr(strip_tags ($topic['description']), 0, 500); ?></a>
+          </th>
+          <th class="date">
               <a href="<?php echo Config::get('config.base'); ?>/forum/Topic/<?php echo $topic['id']; ?>"><?php echo $topic['date_creation']; ?></a>
-          </li>
-          </ul>
-        </div>
-        <?php   $i=$i+1; } ?>
+          </th>
+          </tr>
+                  <?php   $i=$i+1; } ?>
+</table>
