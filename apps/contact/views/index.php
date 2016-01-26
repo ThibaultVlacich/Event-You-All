fcb5dcabd38ef93221536e9277cb25eb439e58fe
Thibault Vlacich <thibault.vlacich@gmail.com>
2016-01-26 21:57:31 +0100

    Pre-fill Contact form is user is logged

--- apps/contact/views/index.php
+++ apps/contact/views/index.php
@@ -1,3 +1,14 @@
+<?php
+  $session = System::getSession();
+
+  if($session->isConnected()) {
+    include_once APPS_DIR.'user'.DS.'model.php';
+
+		$userModel = new UserModel();
+
+    $user = $userModel->getUser($_SESSION['userid']);
+  }
+?>
 <div class="app-contact">
   <h2>Contact</h2>
   <div class="form">
@@ -8,11 +19,11 @@
         <p id="errorS">Votre sujet doit contenir entre 2 et 200 lettres! </p>
         <label for="message">Message <span class="required">*</span></label> <textarea name="message" id="message" required ></textarea>
         <p id="errorM">Votre message doit contenir entre 2 et 5000 lettres! </p>
-        <label for="lastname">Nom <span class="required">*</span></label> <input type="text" name="lastname" id="lastname" required />
+        <label for="lastname">Nom <span class="required">*</span></label> <input type="text" name="lastname" id="lastname" value="<?php echo isset($user) ? $user['lastname'] : ''; ?>" required />
         <p id="errorL">Votre nom doit contenir entre 1 et 40 lettres! </p>
-        <label for="firstname">Prénom <span class="required">*</span></label> <input type="text" name="firstname" id="firstname" required />
+        <label for="firstname">Prénom <span class="required">*</span></label> <input type="text" name="firstname" id="firstname" value="<?php echo isset($user) ? $user['firstname'] : ''; ?>" required />
         <p id="errorF">Votre sujet doit contenir entre 1 et 40 lettres! </p>
-        <label for="email">Adresse e-mail <span class="required">*</span></label> <input type="email" name="email" id="email" required />
+        <label for="email">Adresse e-mail <span class="required">*</span></label> <input type="email" name="email" id="email" value="<?php echo isset($user) ? $user['email'] : ''; ?>" required />
         <p id="errorEM">Ceci n'est pas une adresse e-mail! </p>
         <p>* : champs obligatoires</p>
 
