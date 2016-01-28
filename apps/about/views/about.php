a230806a70c7d222e4a9494398807de08e2a0c9c
Thibault Vlacich <thibault.vlacich@gmail.com>
2016-01-28 22:00:37 +0100

    Improves "about" page

--- apps/about/views/about.php
+++ apps/about/views/about.php
@@ -1,9 +1,6 @@
-
-
-
-            <h2>A propos d' Event-You-All</h2>
-
-        <p id="présentation"><?php /*Considérée comme la plus grande communauté mondiale d'amateur d'arts Event-You-All est un site non lucratif 
-        regroupant des evenements artistiques (concerts, expositions, etc...) dans le monde entier afin de réunir des gens ayant une passion
-        commune pour le plus grand plaisir des petits et des grands. */echo $model['about'];?></p>
-        <img class='event' src="<?php echo Config::get('config.base')?>/apps/about/Image/logo.png" alt="Logo Event-You-All" />
+<div class="app-about">
+  <h2 class="title">A propos d'Event-You-All</h2>
+  <div class="text">
+    <?php echo $model['about'];?>
+  </div>
+</div>
